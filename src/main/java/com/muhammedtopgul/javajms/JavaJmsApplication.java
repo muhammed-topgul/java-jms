faaf413601a0485792705136feda9440bf5116db
muhammed-topgul <muhammedtopgul.29@gmail.com>
2022-02-25 16:27:41 +0300

    java-jms application created.

---
+++ src/main/java/com/muhammedtopgul/javajms/JavaJmsApplication.java
@@ -0,0 +1,13 @@
+package com.muhammedtopgul.javajms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/muhammedtopgul/javajms/JavaJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muhammedtopgul.javajms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
